29893fa4b840fc64d0fb30fc1190260ef5cac73d
Marina Boikova <boymar84@gmail.com>
2021-11-09 14:52:53 +0200

    Изменение решении Задачи 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,25 @@
-const inputRef = document.querySelector('#name-input');
-const outputNameRef = document.querySelector('#name-output');
+const refs = {
+    input: document.querySelector('#name-input'),
+    output: document.querySelector('#name-output')
+};
 
-const handleNameInput = (event) => {
-outputNameRef.textContent = event.currentTarget.value;
-}
+refs.input.addEventListener('input', () => {
 
-inputRef.addEventListener('input', handleNameInput);
+    if(refs.input.value === ""){
+        refs.output.textContent = 'Anonymous';
+        return;
+    }
+    refs.output.textContent = refs.input.value.trim();
+});
+
+
+
+
+// const inputRef = document.querySelector('#name-input');
+// const outputNameRef = document.querySelector('#name-output');
+
+// const handleNameInput = (event) => {
+//         outputNameRef.textContent = event.currentTarget.value;
+//     };
+   
+// inputRef.addEventListener('input', handleNameInput);
