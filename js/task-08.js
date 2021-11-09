f2ad167b694a68ce2ba258cea79aa7401d2faa51
Marina Boikova <boymar84@gmail.com>
2021-11-09 21:02:01 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const refs = {
+    form: document.querySelector('.login-form'),
+    email: document.querySelector('label[name="email"]'),
+    password: document.querySelector('label[name="password"]'),
+    submit: document.querySelector('button[type="submit"]')
+}
+
+const onFormSubmit = evt => {
+evt.preventDefault();
+
+const passwordInput = refs.form.elements.password;
+const emailInput = refs.form.elements.email;
+
+const userEmail = Object.fromEntries(new FormData(refs.form));
+console.log(userEmail);
+
+if (emailInput.value === '' || passwordInput.value === ''){
+alert('all fields must be filled');
+}
+refs.form.reset();
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+
 
