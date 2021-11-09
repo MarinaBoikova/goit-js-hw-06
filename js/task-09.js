ab4e61ecec4212007176a82ae7aef9a03d31f439
Marina Boikova <boymar84@gmail.com>
2021-11-09 22:31:38 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  color: document.querySelector('.color'),
+  changeColor: document.querySelector('.change-color'),
+}
+const onChangeColorClick = evn => {
+  const color = getRandomHexColor();
+  refs.body.style.backgroundColor = color;
+  refs.color.textContent = color;
+}
+refs.changeColor.addEventListener('click', onChangeColorClick);
